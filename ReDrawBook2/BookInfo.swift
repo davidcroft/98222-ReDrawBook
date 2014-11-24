299d14f9be8635f40022ab1fc0ab5fcd07276847
davidcroft <dingxu@Dings-MBP.wv.cc.cmu.edu>
2014-11-23 22:33:53 -0500

    finish reading from parse

---
+++ ReDrawBook2/BookInfo.swift
@@ -0,0 +1,13 @@
+//
+//  BookInfo.swift
+//  ReDrawBook2
+//
+//  Created by Ding Xu on 11/23/14.
+//  Copyright (c) 2014 Ding Xu. All rights reserved.
+//
+
+import Cocoa
+
+class BookInfo: NSObject {
+
+}
